83730216ddddd4471431dca5d952bbe31bc32b6c
zgoins <zgoins@ufl.edu>
2015-11-05 19:18:42 -0500

    sdfsdfa

--- PA3.cpp
+++ PA3.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,6 +19,7 @@ public:
 	void pop();
 	void print_heap();
 	int get_size(){ return size; };
+    int top(){ return heap_array[1]; };
 	bool empty();
 	int size;
 	int *heap_array;
@@ -31,13 +34,13 @@ Min_heap::Min_heap(int num_elements)
 }
 
 Min_heap::~Min_heap(){
-
+    //delete heap_array;
 }
 
 void Min_heap::build_minheap()
 {
 
-    for(int i = num_elm/2; i >= 1; i--)
+    for(int i = size/2; i >= 1; --i)
     {
         min_heapify(i);
     }
@@ -45,12 +48,15 @@ void Min_heap::build_minheap()
 
 void Min_heap::min_heapify(int i)
 {
+    sort(heap_array, heap_array + size);
+    
+    
     int j, temp;
     temp = heap_array[i];
     j = 2 * i;
     while (j <= num_elm)
     {
-        if (j < num_elm && heap_array[j+1] < heap_array[j])
+        if (j < num_elm && heap_array[j] < heap_array[j+1])
             j = j + 1;
         if (temp < heap_array[j])
             break;
@@ -59,12 +65,11 @@ void Min_heap::min_heapify(int i)
             heap_array[j/2] = heap_array[j];
             j = 2 * j;
         }
-
-
-    cout << i << endl;
     }
     heap_array[j/2] = temp;
+    print heap_array[i/2]
     return;
+    
 }
 
 void Min_heap::push(int element){
@@ -74,17 +79,11 @@ void Min_heap::push(int element){
 
 void Min_heap::pop()
 {
-    for(int i = 0; i <= size; i++)
+    for(int i = 1; i <= size; i++)
     {
         heap_array[i] = heap_array[i+1];
     }
-
     size--;
-
-    for(int i = size/2; i > 1; i--)
-    {
-        min_heapify(i);
-    }
 }
 
 void Min_heap::print_heap(){
@@ -99,7 +98,7 @@ void Min_heap::print_heap(){
 
 bool Min_heap::empty()
 {
-	if (size > 0)
+	if (size > 1)
 	{
 		return true;
 	}else{
@@ -109,46 +108,75 @@ bool Min_heap::empty()
 
 int main(int argc, char** argv)
 {
+
+    clock_t start;
+    double duration;
+    start = clock();
+
 	
 
 	int num_elements, num_machine, x;
 
-    cout << "enter number of jobs\n";
+    /*
+
+    cout << "Enter number of jobs\n";
     cin >> num_elements;
-    cout << "enter number of machines\n";
+    cout << "Enter number of machines\n";
     cin >> num_machine;
 
     Min_heap min_heap(num_elements);
 
-    /*
-
     for (int i = 1; i <= num_elements; i++)
     {
-    	int input;
+        int input;
         cout << "Enter element " << (i) << endl;
         cin >> input;
         min_heap.push(input);
     }
-
     */
-
-    min_heap.push(14);
-    min_heap.push(7);
-    min_heap.push(10);
-    min_heap.push(6);
-    min_heap.push(2);
+    
+    num_elements = 7;
+    num_machine = 3;
+    Min_heap min_heap(num_elements);
     min_heap.push(3);
-    min_heap.push(5);
-
-
-
+    min_heap.push(10);
+    min_heap.push(1);
+    min_heap.push(11);
+    min_heap.push(13);
+    min_heap.push(17);
+    min_heap.push(6);
+    
 
     min_heap.build_minheap();
     min_heap.print_heap();
 
 
+
+    /*
+
+    int index = 1;
+    while(min_heap.empty() != false)
+    {
+        cout << "Machine " << index << ": ";
+        for (int i = 1; i <= num_machine; i++)
+        {
+            if (min_heap.empty() == false){ break; };
+            int temp = min_heap.top();
+            cout << temp << ", ";
+            min_heap.pop();
+        }
+        cout << endl;
+        index++;
+    }
+    */
+
+
+
     
 
+    duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
+
+    cout<<"Time Elapsed: "<< duration <<'\n';
 
 }
 
